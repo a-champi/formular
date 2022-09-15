42b4ab7b166974d8c9f3d6730858882d0430b717
a-champi <a.champi.h@gmail.com>
2022-09-15 06:59:48 +0200

    finisch

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # formular
+This form was implemented using JavaScript, HTML5 and Bootstrap.
+
+### Terms:
+- form
+- input
+- regular Expresion
+- DOM (__D__ ocument __O__ bject __M__ odell)
+- 
--- app.js
+++ app.js
@@ -1,15 +1,63 @@
+// const userName = document.querySelector("input[name='userName']");
+// const userEmail = document.querySelector("input[name='userEmail']");
+
 const form = document.getElementById("form");
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
 
-// const userName = document.getElementById("userName");
-// const userEmail = document.getElementById("userEmail");
+const alertUserName = document.getElementById("alertUserName");
+const alertUserEmail = document.getElementById("alertUserEmail");
+const alertSuccess = document.getElementById("alertSuccess");
 
-const userName = document.querySelector("input[name='userName']");
-const userEmail = document.querySelector("input[name='userEmail']");
+const showAlertOk = () =>{
+    alertSuccess.classList.remove("d-none");
+    alertSuccess.textContent = "email is sended allready..!";
+}
+const showAlertError = (errors) =>{
+    errors.forEach(element => {
+      element.type.classList.remove("d-none");
+      element.type.textContent = element.msg;    
+
+    });
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("Username: ", userName.value);
-    console.log("UserEmail: ", userEmail.value);
+    alertSuccess.classList.add("d-none");
+
+    const errors = [];
+    
+    const regUserName = /^[a-zZ-Z ]*$/;
+    const regUserEmail = /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
     
 
-})
+      if(!regUserName.test(userName.value) || !userName.value.trim()){
+        userName.classList.add("is-invalid");
+        errors.push({
+            type: alertUserName,
+            msg: " write only letters for name"
+        });
+      }else{
+        userName.classList.remove("is-invalid");
+        userName.classList.add("is-valid");
+        alertUserName.classList.add("d-none");
+      }  
+      if(!regUserEmail.test(userEmail.value)|| !userEmail.value.trim()){
+        userEmail.classList.add("is-invalid");
+        errors.push({
+            type: alertUserEmail,
+            msg: " write a valid Email"
+        });
+ 
+      }else{
+        userEmail.classList.remove("is-invalid");
+        userEmail.classList.add("is-valid");
+        alertUserEmail.classList.add("d-none");
+      }
+      if(errors.length !== 0){
+        showAlertError(errors);
+        return;
+      }
+      console.log("sended email!!!!!!")
+    showAlertOk(); 
+});
